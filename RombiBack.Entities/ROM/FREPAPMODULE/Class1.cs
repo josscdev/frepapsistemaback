36bfcd361d4aed3fe3f17e1d44389d4b15c75acd
adriancelistorres <adriancelistorres184@gmail.com>
2025-08-31 16:11:22 -0500

    feat: cambios minimos

---
+++ RombiBack.Entities/ROM/FREPAPMODULE/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RombiBack.Entities.ROM.FREPAPMODULE
+{
+    public class Afiliado
+    {
+        public Guid Id { get; set; }
+
+        public string? NroAfili { get; set; }
+        public DateTime? FechaAfiliacion { get; set; }
+
+        public string Nombres { get; set; } = null!;
+        public string ApPaterno { get; set; } = null!;
+        public string? ApMaterno { get; set; }
+
+        public string TipoDocumento { get; set; }
+        public string NroDocumento { get; set; } = null!;
+
+        public DateTime? FechaNacimiento { get; set; }
+        public int? Edad { get; set; }
+        public int Estado { get; set; } 
+
+        public string? Region { get; set; }
+        public string? Provincia { get; set; }
+        public string? Distrito { get; set; }
+
+        public string? Correo { get; set; }
+        public string? Telefono { get; set; }
+        public string? Observacion { get; set; }
+
+        // --- Firma y Huella (BLOB + metadatos opcionales)
+        public byte[]? Firma { get; set; }
+        public string? FirmaContentType { get; set; }   // image/png, image/jpeg
+        public long? FirmaSize { get; set; }
+
+        public byte[]? Huella { get; set; }
+        public string? HuellaContentType { get; set; }
+        public long? HuellaSize { get; set; }
+    }
+}
---
+++ RombiBack/Controllers/ROM/FREPAPMODULE/GESTIONAFILIACIONES/MODULOREGISTROS/AfiliacionesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RombiBack.Controllers.ROM.FREPAPMODULE.GESTIONAFILIACIONES.MODULOREGISTROS
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AfiliacionesController : ControllerBase
+    {
+    }
+}
